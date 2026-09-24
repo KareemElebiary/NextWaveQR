7ce8c14d3cca8c5bac964424b3e0473a5aca95f7
KareemElebiary <kareemelebiary@outlook.com>
2026-09-24 12:16:46 +0300

    Update attendance web app URL

--- scanner/config.js
+++ scanner/config.js
@@ -1,2 +1,2 @@
 // Apps Script web app endpoint used to mark attendance in Google Sheets.
-export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwy5yDhxQuBElYpuTfXwQjAuuRCg2Y1tiH5Qf6q08svYlXAZgrQCo7xxM8EOjpVGjJWAg/exec";
+export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbynvWpjcgiO9Ql3rNJDbgGDBwAlWFyyfvj5ai1v5jWG0Lx6upGMkW9HF0BIVQSpZlHtaw/exec";
