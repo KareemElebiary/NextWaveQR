e2b30da3c65226b6b57d48452284759ee0bff4bb
KareemElebiary <kareemelebiary@outlook.com>
2026-09-24 11:36:40 +0300

    Update APPS_SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js – replace with your Apps Script Web App URL
-export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyNt9N-7bxdAlLREAaW2FEMXdzkqMBGcj_imf0vn2ceYY14nhlq78J4kahEpHTINIZ-HA/exec";
+export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwy5yDhxQuBElYpuTfXwQjAuuRCg2Y1tiH5Qf6q08svYlXAZgrQCo7xxM8EOjpVGjJWAg/exec";
